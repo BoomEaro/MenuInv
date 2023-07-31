44239bbc8fb01f9631c135874d23e071e9fd7ba1
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2023-07-31 12:50:14 +0300

    Improve InventoryListener

--- src/main/java/ru/boomearo/menuinv/listeners/InventoryListener.java
+++ src/main/java/ru/boomearo/menuinv/listeners/InventoryListener.java
@@ -23,17 +23,20 @@ public class InventoryListener implements Listener {
 
     private static final int OUTSIDE = -999;
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onInventoryClickEvent(InventoryClickEvent e) {
-        if (e.isCancelled()) {
-            return;
-        }
-        if (!(e.getWhoClicked() instanceof Player)) {
+        InventoryView view = e.getView();
+        if (view == null) {
             return;
         }
 
-        Inventory topInventory = e.getView().getTopInventory();
+        Inventory topInventory = view.getTopInventory();
         if (topInventory == null) {
+            return;
+        }
+
+        Inventory clickedInventory = e.getClickedInventory();
+        if (clickedInventory == null) {
             return;
         }
 
@@ -42,17 +45,15 @@ public class InventoryListener implements Listener {
             return;
         }
 
-        MenuInventoryHolder menuHolder = (MenuInventoryHolder) holder;
-
-        Inventory clickedInventory = e.getClickedInventory();
-        if (clickedInventory == null) {
+        if (!(e.getWhoClicked() instanceof Player)) {
             return;
         }
 
         // Allowing the player to modify their inventory
-        if (e.getView().getBottomInventory() == clickedInventory) {
+        if (view.getBottomInventory() == clickedInventory) {
             InventoryAction action = e.getAction();
 
+            // TODO More InventoryActions to block?
             if (action == InventoryAction.MOVE_TO_OTHER_INVENTORY || action == InventoryAction.COLLECT_TO_CURSOR) {
                 e.setCancelled(true);
             }
@@ -60,31 +61,34 @@ public class InventoryListener implements Listener {
             return;
         }
 
-        // Now we cancel the event if it a menu
-
+        // Now we cancel the event if it is a menu
         e.setCancelled(true);
+
+        MenuInventoryHolder menuHolder = (MenuInventoryHolder) holder;
 
         InventoryPageImpl page = menuHolder.getPage();
 
         page.handleInventoryClick(e.getSlot(), e.getClick());
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onInventoryDragEvent(InventoryDragEvent e) {
-        if (e.isCancelled()) {
-            return;
-        }
-        if (!(e.getWhoClicked() instanceof Player)) {
-            return;
-        }
-
         InventoryView view = e.getView();
+
+        if (view == null) {
+            return;
+        }
 
         Inventory topInventory = view.getTopInventory();
         if (topInventory == null) {
             return;
         }
+
         if (!(topInventory.getHolder() instanceof MenuInventoryHolder)) {
+            return;
+        }
+
+        if (!(e.getWhoClicked() instanceof Player)) {
             return;
         }
 
@@ -103,14 +107,18 @@ public class InventoryListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onInventoryCloseEvent(InventoryCloseEvent e) {
-        if (!(e.getPlayer() instanceof Player)) {
+        InventoryView view = e.getView();
+
+        if (view == null) {
             return;
         }
 
-        InventoryView view = e.getView();
-
         Inventory topInventory = view.getTopInventory();
         if (topInventory == null) {
+            return;
+        }
+
+        if (!(e.getPlayer() instanceof Player)) {
             return;
         }
 
@@ -132,7 +140,7 @@ public class InventoryListener implements Listener {
         Menu.unregisterPages(e.getPlugin());
     }
 
-    // Copied from newer version to backend compatability
+    // Copied from newer version for backend compatability
     private static Inventory getInventory(InventoryView view, int rawSlot) {
         // Slot may be -1 if not properly detected due to client bug
         // e.g. dropping an item into part of the enchantment list section of an enchanting table
